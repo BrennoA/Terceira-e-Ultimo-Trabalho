572c90d23a42c33e9f6eeb4d3e25e7cffc15100b
BrennoA <82546833+BrennoA@users.noreply.github.com>
2021-06-22 23:26:05 -0300

    Create banco.php

---
+++ banco.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+
+$localhost = "localhost";
+$user = "root";
+$passw = "";
+$banco = "av3bd";
+
+global $pdo;
+
+try{
+
+    $pdo = new PDO("mysql:dbname=".$banco."; host=".$localhost, $user, $passw);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch(PDOException $e){
+    echo "ERRO: ". $e->getMessage();
+    exit;
+}catch(Exception $e){
+    echo "ERRO GENERICO: ".$e->getMessage();;
+    exit; 
+}
+
+
+//$sql = $pdo->query("SELECT * FROM usuario");
+//$sql->execute();
+
+//echo $sql->rowCount();
+
+?>
